fa82bb2cd1edc0c437eaf1679c72ab661188b3f1
Mark Huang <git@hyxi.dev>
2025-08-19 16:45:49 +0800

    refactor: improve event listener management by supporting `CustomChromeEvent`

--- src/shared/listeners.ts
+++ src/shared/listeners.ts
@@ -1,26 +1,53 @@
-export class Listeners {
-	private listeners: Map<api.events.Event<any>, Function[]> = new Map();
+interface Callback {
+	function: (...args: any[]) => void | Promise<void>;
+	args: any[];
+}
 
+type CustomChromeEvent<H extends (...args: any) => void> =
+	Omit<api.events.Event<H>, 'addListener'> & {
+		readonly addListener: H;
+	};
+
+export class Listeners {
+	private listeners: Map<CustomChromeEvent<any>, Callback[]> = new Map();
+
+	public add<H extends (...args: any[]) => void>(
+		event: CustomChromeEvent<H>,
+		...args: Parameters<H>
+	): void;
 	public add<T extends api.events.Event<any>>(
 		event: T,
 		callback: T extends api.events.Event<infer H> ? H : never,
+	): void;
+	public add<H extends (...args: any[]) => void>(
+		event: CustomChromeEvent<H>,
+		callback: H,
+		...args: any[]
 	): void {
 		if (!this.listeners.has(event)) {
 			this.listeners.set(event, []);
 		}
-		this.listeners.get(event)?.push(callback);
+		this.listeners.get(event)?.push({
+			function: callback,
+			args: args,
+		});
 	}
 
 	public resolve(): void {
 		for (const [event, callbacks] of this.listeners.entries()) {
-			event.addListener(async (...args: any[]) => {
-				for (const callback of callbacks) {
-					const ret = callback(...args);
+			const noArgsCallbacks = callbacks.filter(cb => cb.args.length === 0);
+			event.addListener(async (..._args: any[]) => {
+				for (const callback of noArgsCallbacks) {
+					const ret = callback.function(..._args);
 					if (ret instanceof Promise) {
 						await ret;
 					}
 				}
 			});
+			const argsCallbacks = callbacks.filter(cb => cb.args.length > 0);
+			for (const callback of argsCallbacks) {
+				event.addListener(callback.function, ...callback.args);
+			}
 		}
 	}
 }
